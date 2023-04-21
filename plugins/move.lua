9b37b74f0e2274d5abafec71617f61db6ace366d
Mon <55015209+Mondotosz@users.noreply.github.com>
2023-04-21 11:29:47 +0200

    Configured default extensions and added move

---
+++ plugins/move.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "fedepujol/move.nvim",
+    keys = {
+      { "<A-j>", ":MoveLine(1)<CR>" },
+      { "<A-k>", ":MoveLine(-1)<CR>" },
+      { "<A-l>", ":MoveHChar(1)<CR>" },
+      { "<A-h>", ":MoveHChar(-1)<CR>" },
+    },
+  },
+}
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,22 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = {
+      "lua",
+      "rust",
+      "javascript",
+      "typescript",
+      "php",
+      "json",
+      "yaml",
+      "toml",
+      "vue",
+      "python",
+      "markdown",
+      "latex",
+      "html",
+      "dockerfile",
+      "css",
+    },
   },
 }
