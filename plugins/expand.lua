f11d744734ad3882691dc41e8949c7505aa64bff
Mondotosz <55015209+Mondotosz@user.noreply.github.com>
2023-04-26 15:21:12 +0200

    switched to lazy loading

--- plugins/expand.lua
+++ plugins/expand.lua
@@ -1,6 +1,5 @@
 return {
   "terryma/vim-expand-region",
-  lazy = false,
   keys = {
     { "+", "<Plug>(expand_region_expand)", mode = { "n", "v" } },
     { "_", "<Plug>(expand_region_shrink)", mode = { "n", "v" } },
