c1e330eda55d963ff98222a91cf1166702230ede
Mondotosz <55015209+Mondotosz@user.noreply.github.com>
2023-04-28 11:54:42 +0200

    reenabled auto_trigger for copilot and removed tab to accept

--- plugins/community.lua
+++ plugins/community.lua
@@ -26,7 +26,7 @@ return {
     "copilot.lua",
     opts = {
       suggestion = {
-        auto_trigger = false,
+        debounce = 200,
       },
     },
   },
@@ -34,7 +34,7 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = { "zbirenbaum/copilot.lua" },
     opts = function(_, opts)
-      local cmp, copilot = require "cmp", require "copilot.suggestion"
+      local cmp = require "cmp"
       local snip_status_ok, luasnip = pcall(require, "luasnip")
 
       if not snip_status_ok then return end
@@ -50,8 +50,6 @@ return {
       opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif copilot.is_visible() then
-          copilot.accept()
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
         elseif has_words_before() then
@@ -60,14 +58,6 @@ return {
           fallback()
         end
       end, { "i", "s" })
-
-      -- opts.mapping["<C-x>"] = cmp.mapping(function()
-      --   if copilot.is_visible() then copilot.next() end
-      -- end)
-
-      -- opts.mapping["<C-z>"] = cmp.mapping(function()
-      --   if copilot.is_visible() then copilot.prev() end
-      -- end)
 
       return opts
     end,
